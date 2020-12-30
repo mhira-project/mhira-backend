f91e3f9123d2ddc98b5d7b2dcd61b4a816969036
Tumaini Mosha <tumaini@codeblock.co.tz>
2020-12-30 17:15:27 +0300

    Dynamic user validation

--- src/modules/user/resolvers/user-crud.resolver.ts
+++ src/modules/user/resolvers/user-crud.resolver.ts
@@ -1,19 +1,39 @@
 import { UpdateManyResponse, Filter, SortDirection } from '@nestjs-query/core';
-import { CRUDResolver, FilterType, UpdateManyResponseType } from '@nestjs-query/query-graphql';
-import { Resolver, Args, Mutation, ID, ResolveField, Parent } from '@nestjs/graphql';
+import { CreateOneInputType, CRUDResolver, FilterType, UpdateManyResponseType } from '@nestjs-query/query-graphql';
+import { BadRequestException } from '@nestjs/common';
+import { Resolver, Args, Mutation, ID, ResolveField, Parent, InputType } from '@nestjs/graphql';
 import { CreateUserInput } from '../dto/create-user.input';
 import { UpdateUserInput } from '../dto/update-user.input';
 import { User } from '../models/user.model';
 import { UserCrudService } from '../providers/user-crud.service';
 
+@InputType()
+export class CreateOneUserInput extends
+    CreateOneInputType('user', CreateUserInput) {
+
+}
 @Resolver(() => User)
 export class UserCrudResolver extends CRUDResolver(User, {
     CreateDTOClass: CreateUserInput,
     UpdateDTOClass: UpdateUserInput,
     read: { defaultSort: [{ field: 'id', direction: SortDirection.DESC }] },
+    create: { disabled: true }
 }) {
     constructor(readonly service: UserCrudService) {
         super(service);
+    }
+
+    // // restore one mutation will update the `deletedAt` column to null.
+    @Mutation(() => User)
+    async createOneUser(@Args('input', { type: () => CreateOneUserInput }) input: CreateOneUserInput): Promise<User> {
+
+        const exists = await User.findOne({ username: input['user'].username });
+
+        if (exists) {
+            throw new BadRequestException('Username already taken');
+        }
+
+        return this.service.createOne(input['user']);
     }
 
     // restore one mutation will update the `deletedAt` column to null.
