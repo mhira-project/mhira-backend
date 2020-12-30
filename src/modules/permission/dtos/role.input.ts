f16b7b055516fb1822aa0be49c1ce03a8c4697cf
Tumaini Mosha <tumaini@codeblock.co.tz>
2020-12-30 16:29:06 +0300

    Fix role name validation

--- src/modules/permission/dtos/role.input.ts
+++ src/modules/permission/dtos/role.input.ts
@@ -1,11 +1,11 @@
 import { InputType, Field } from "@nestjs/graphql";
-import { Max } from "class-validator";
+import { MaxLength } from "class-validator";
 import { GuardType } from "../models/guard-type.enum";
 
 @InputType()
 export class RoleInput {
 
-    @Max(15)
+    @MaxLength(15)
     @Field()
     name: string;
 
