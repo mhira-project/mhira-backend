b76f3fb8daf9b7d3c24bf544d7c3f8df34f2c1c0
Natalie Schumacher <snatsch@gmx.ch>
2021-01-30 00:15:25 +0100

    add unused prefix and make validate statement shorter

--- src/shared/decorators/is-optional.ts
+++ src/shared/decorators/is-optional.ts
@@ -1,10 +1,7 @@
-
 import { ValidationOptions, ValidateIf } from 'class-validator';
 
 export function IsOptional(validationOptions?: ValidationOptions) {
-    return ValidateIf((obj, value) => {
-        const isValid = value !== null && value !== undefined && value !== '';
-
-        return !!isValid;
+    return ValidateIf((_obj, value) => {
+        return !!value;
     }, validationOptions);
 }
