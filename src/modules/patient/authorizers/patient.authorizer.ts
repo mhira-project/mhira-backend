436c923b58ada000917bbb272c5a647b77840ac4
Tumaini Mosha <tumaini@codeblock.co.tz>
2021-04-12 21:08:12 +0300

    Refactored patient authorizer

--- src/modules/patient/authorizers/patient.authorizer.ts
+++ src/modules/patient/authorizers/patient.authorizer.ts
@@ -3,41 +3,24 @@ import { UserContext } from "src/modules/auth/interfaces/auth-interfaces";
 import { Filter } from '@nestjs-query/core';
 import { Patient } from "../models/patient.model";
 import { User } from "src/modules/user/models/user.model";
-import { Logger } from "@nestjs/common";
-import { ForbiddenError } from "apollo-server-errors";
-
 
 export class PatientAuthorizer implements Authorizer<Patient> {
 
-    protected readonly logger = new Logger(PatientAuthorizer.name);
 
     async authorize(context: UserContext): Promise<Filter<Patient>> {
 
-        // Reload current user with departments
-        const currentUser = await User.findOne({
-            where: { id: context.req.user.id },
-            relations: ['departments']
-        });
-
-        const deparmentIds = currentUser.departments.map((department) => department.id);
-
-        // User has no departments
-        if (deparmentIds.length < 1) {
-
-            Promise.resolve(
-                { departments: { id: { is: null } } } // include patients without a department
-            );
-        }
-
-        return Promise.resolve({
-            or: [
-                { departments: { id: { in: deparmentIds } } }, // include patients from own departments
-                { departments: { id: { is: null } } } // include patients without a department
-            ]
-        });
+        // delegates to `authorizePatient` method
+        return this.authorizePatient(context);
     }
 
-    async authorizeRelation(relationName: string, context: UserContext): Promise<Filter<unknown>> {
+    async authorizeRelation(relationName: string, context: UserContext): Promise<Filter<Patient>> {
+
+        // delegates to `authorizePatient` method
+        return this.authorizePatient(context);
+    }
+
+    protected async authorizePatient(context: UserContext): Promise<Filter<Patient>> {
+
         // Reload current user with departments
         const currentUser = await User.findOne({
             where: { id: context.req.user.id },
@@ -56,8 +39,8 @@ export class PatientAuthorizer implements Authorizer<Patient> {
 
         return Promise.resolve({
             or: [
-                { departments: { id: { in: deparmentIds } } }, // include patients from own departments
-                { departments: { id: { is: null } } } // include patients without a department
+                { departments: { id: { in: deparmentIds } } },
+                { departments: { id: { is: null } } }
             ]
         });
     }
