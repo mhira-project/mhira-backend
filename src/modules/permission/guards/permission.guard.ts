a450c77b076180ae98a236343aaa79f752444717
Markus Chiarot <cmarkus@hotmail.ch>
2021-04-19 09:45:29 +0200

    fix(permissions): implement PR recommendations

--- src/modules/permission/guards/permission.guard.ts
+++ src/modules/permission/guards/permission.guard.ts
@@ -18,18 +18,19 @@ export class PermissionGuard implements CanActivate {
         // Return failure if un-authenticated request
         if (!userId) return false;
 
-        const permissions = this.getPermissions(context);
-        const orPermissions = this.getOrPermissions(context);
+        // get permissions of user
         const permissionGrants = await PermissionService.userPermissionGrants(userId);
 
         // Check each required permission is available on User
         // Return failure on first missing permission
+        const permissions = this.getPermissions(context, 'permissions');
         for (const permission of permissions) {
             if (permissionGrants.find(p => p.name === permission)) continue;
             throw new ForbiddenError(`Forbidden! Permission '${permission}' required to access this resource`);
         }
 
         // Check if one of the or_permissions is available on User
+        const orPermissions = this.getPermissions(context, 'or_permissions');
         if (orPermissions.length && !orPermissions.some(p => permissionGrants.find(pg => pg.name === p))) {
             throw new ForbiddenError(`Forbidden! At least one of the permissions '${orPermissions.join(', ')}' is required to access this resource`);
         }
@@ -37,23 +38,13 @@ export class PermissionGuard implements CanActivate {
         return true;
     }
 
-    private getOrPermissions(context: ExecutionContext): string[] {
+    private getPermissions(context: ExecutionContext, type: 'permissions' | 'or_permissions'): string[] {
         return [
             // Gets Anotations defined on method level
-            this.reflector.get<string[] | string>("or_permissions", context.getHandler()) ?? [],
+            this.reflector.get<string[] | string>(type, context.getHandler()) ?? [],
 
             // Gets Anotations defined on Controller/Resolver class level
-            this.reflector.get<string[] | string>("or_permissions", context.getClass()) ?? [],
-        ].flat();
-    }
-
-    private getPermissions(context: ExecutionContext): string[] {
-        return [
-            // Gets Anotations defined on method level
-            this.reflector.get<string[] | string>("permissions", context.getHandler()) ?? [],
-
-            // Gets Anotations defined on Controller/Resolver class level
-            this.reflector.get<string[] | string>("permissions", context.getClass()) ?? [],
+            this.reflector.get<string[] | string>(type, context.getClass()) ?? [],
         ].flat();
     }
 }
