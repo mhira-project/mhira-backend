eb9a3bfd1a13aa431309c43f137143a699edf30f
Tumaini Mosha <tumaini@codeblock.co.tz>
2021-05-11 15:45:13 +0300

    WIP: initial schema for permissions grouping

--- src/modules/permission/enums/permission.enum.ts
+++ src/modules/permission/enums/permission.enum.ts
@@ -1,5 +1,36 @@
 
 
+export const Permissions = {
+
+    MANAGE_USERS: { value: 'manage users', group: 'User Management' },
+    VIEW_USERS: { value: 'view users', group: 'User Management' },
+    DELETE_USERS: { value: 'delete users', group: 'User Management' },
+
+    MANAGE_PATIENTS: { value: 'manage patients', group: 'Patient Management' },
+    VIEW_PATIENTS: { value: 'view patients', group: 'Patient Management' },
+    DELETE_PATIENTS: { value: 'delete patients', group: 'Patient Management' },
+
+    VIEW_SYSCONFIG: { value: 'view sysconf', group: 'System Configuration' },
+    MANAGE_SYSCONFIG: { value: 'manage sysconf', group: 'System Configuration' },
+
+    VIEW_SETTINGS: { value: 'view settings', group: 'System Configuration' },// this could be redundant, as all users need to access settings
+    MANAGE_SETTINGS: { value: 'manage settings', group: 'System Configuration' },
+
+    VIEW_QUESTIONNAIRES: { value: 'view questionnaires', group: 'Questionnaires' },
+    MANAGE_QUESTIONNAIRES: { value: 'manage questionnaires', group: 'Questionnaires' },
+    DELETE_QUESTIONNAIRES: { value: 'delete questionnaires', group: 'Questionnaires' },
+
+    VIEW_ASSESSMENTS: { value: 'view assessments', group: 'Assessments' },
+    MANAGE_ASSESSMENTS: { value: 'manage assessments', group: 'Assessments' },
+    DELETE_ASSESSMENTS: { value: 'delete assessments', group: 'Assessments' },
+
+    VIEW_REPORTING_TOOLS: { value: 'view reporting_tools', group: 'Reporting Tools' },
+    MANAGE_REPORTING_TOOLS: { value: 'manage reporting_tools', group: 'Reporting Tools' },
+
+    VIEW_ROLES_PERMISSIONS: { value: 'view roles_permissions', group: 'Roles and Permissions' },
+    MANAGE_ROLES_PERMISSIONS: { value: 'manage roles_permissions', group: 'Roles and Permissions' },
+
+}
 
 export enum PermissionEnum {
 
