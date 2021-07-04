365df113ba0e66d6c5ba5d9dc664999e1b19f004
Markus Chiarot <cmarkus@hotmail.ch>
2021-07-04 23:34:16 +0200

    fix: throw correct error on answer validation, add some validation

--- src/modules/questionnaire/validation/choice.validation.ts
+++ src/modules/questionnaire/validation/choice.validation.ts
@@ -1,17 +1,25 @@
 import { Answer } from '../models/answer.schema';
 import { QuestionType } from '../models/question.schema';
 import { BasicValidator } from './basic.validation';
+import { UserInputError } from 'apollo-server-express';
 
 export class ChoiceQuestionValidator extends BasicValidator {
     protected validate(answer: Answer) {
         const choices = this.question.choices.map(choice => choice.name);
+
+        if (this.question.required && (
+            (this.question.type === QuestionType.SELECT_ONE && (answer.textValue === null || answer.textValue === undefined)) ||
+            (this.question.type === QuestionType.SELECT_MULTIPLE && !answer.multipleChoiceValue?.length)
+        )) {
+            throw new UserInputError('Answer cannot be empty');
+        }
 
         if (
             answer.multipleChoiceValue &&
             (answer.multipleChoiceValue.length < this.question.min ||
                 answer.multipleChoiceValue.length > this.question.max)
         ) {
-            throw new Error(
+            throw new UserInputError(
                 `Number of answers must be between ${this.question.min} and ${this.question.max}`,
             );
         }
@@ -20,7 +28,7 @@ export class ChoiceQuestionValidator extends BasicValidator {
             (this.question.type === QuestionType.SELECT_ONE && !choices.includes(answer.textValue)) ||
             (this.question.type === QuestionType.SELECT_MULTIPLE && answer.multipleChoiceValue?.some(av => !choices.includes(av)))
         ) {
-            throw new Error(`Please only choose available answers!`);
+            throw new UserInputError(`Please only choose available answers!`);
         }
     }
 }
--- src/modules/questionnaire/validation/number.validation.ts
+++ src/modules/questionnaire/validation/number.validation.ts
@@ -1,10 +1,12 @@
 import { Answer } from '../models/answer.schema';
 import { BasicValidator } from './basic.validation';
+import { UserInputError } from 'apollo-server-express';
 
 export class NumberQuestionValidator extends BasicValidator {
     protected validate(answer: Answer): void {
+        if (this.question.required && answer.numberValue === null) throw new UserInputError('Answer cannot be empty');
         if (this.question.max && answer.numberValue > this.question.max) {
-            throw new Error(
+            throw new UserInputError(
                 `Number given for question ${this.question.name} is too high. Can only be ${this.question.max} or below.`,
             );
         }
