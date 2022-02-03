b8f74604000f30737fe600cdf0605239bc50336e
leonard <leonard9619@gmail.com>
2022-02-03 16:04:36 +0100

    ts lint fix

--- src/migrations/1643899056854-caregivers.ts
+++ src/migrations/1643899056854-caregivers.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class caregivers1643899056854 implements MigrationInterface {
+export class Caregivers1643899056854 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
