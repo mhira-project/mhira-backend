8b9c95a2922cccfd3284d8e9949ed353c9783e7d
leonard <leonard9619@gmail.com>
2022-02-10 16:56:09 +0100

    remove unused importS

--- src/modules/assessment/resolvers/public.assessment.resolver.ts
+++ src/modules/assessment/resolvers/public.assessment.resolver.ts
@@ -1,9 +1,6 @@
 import { Resolver, Query, Args } from '@nestjs/graphql';
 import { Assessment, FullAssessment } from '../models/assessment.model';
 import { AssessmentService } from '../services/assessment.service';
-import { UsePermission } from '../../permission/decorators/permission.decorator';
-import { PermissionEnum } from '../../permission/enums/permission.enum';
-
 
 @Resolver(() => Assessment)
 export class PublicAssessmentResolver {
